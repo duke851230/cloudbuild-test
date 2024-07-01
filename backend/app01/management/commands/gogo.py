07125ae93986ef6a4acfdaae2e3782ac631bce84
Duke <as124122323@gmail.com>
2024-07-01 16:58:14 +0800

    Test: Test build image in cicd

---
+++ backend/app01/management/commands/gogo.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        print(1111111111111111111111)
+        print(1111111111111111111111)
+        print(1111111111111111111111)
