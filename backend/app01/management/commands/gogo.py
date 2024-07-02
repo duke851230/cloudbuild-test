10b1813bc16a361f73f1769e92af4f82a13065e6
Duke <as124122323@gmail.com>
2024-07-02 12:16:55 +0800

    Test: no waitFor in cicd

--- backend/app01/management/commands/gogo.py
+++ backend/app01/management/commands/gogo.py
@@ -4,4 +4,5 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        print("------------ start run gogo command ------------")
         print("test whether command run successfully or not.")
